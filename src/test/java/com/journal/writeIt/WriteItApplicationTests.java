28cce57735da9346a6684ae6ad4465613a220c1b
rankit2001 <rankitgujjar@gmail.com>
2025-06-11 00:56:29 +0530

    project initialise

---
+++ src/test/java/com/journal/writeIt/WriteItApplicationTests.java
@@ -0,0 +1,11 @@
+package com.journal.writeIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteItApplicationTests {
+
+
+
+}
